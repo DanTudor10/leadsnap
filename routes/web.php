434c7698626240587a7d9e136f724efbecdeee7b
DanTudor10 <dan.tudor@webitfactory.io>
2026-01-14 11:46:28 +0200

    feat: add registration and change default route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    if (auth()->check()) {
+        return redirect('/admin'); 
+    }
+
+    return redirect('/admin/login'); 
 });
